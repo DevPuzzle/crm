e4fc0efbe0bff27f717601edf910ba9b7b88cb21
alexander-tarasenko <tarasenkoA.dp@gmail.com>
2019-01-18 16:18:28 +0200

    imported modules to angular-material module

---
+++ client/src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// all angular material modules go here
+const MATERIAL_MODULES = [
+  MatInputModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatRadioModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MATERIAL_MODULES
+  ]
+})
+export class AngularMaterialModule { }
