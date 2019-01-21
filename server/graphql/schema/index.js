8283375b667736f73c1ebe04e5c7737c04b642eb
alex-fedurko <alex.fedurko.dp@gmail.com>
2019-01-21 09:34:08 +0200

    First upload

---
+++ server/graphql/schema/index.js
@@ -0,0 +1,38 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        company_id: String
+    }
+
+    type UserData {
+        status: String
+        email: String!
+        name: String!
+        company_id: String
+    }
+
+    input UserInputData {
+        email: String!
+        name: String
+        password: String!
+        company_id: String
+    }
+
+    type RootQuery {
+        getUser: UserData!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
