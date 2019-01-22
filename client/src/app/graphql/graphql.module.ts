f230c20dcf392444f52fb0463e03826c9443d04d
alexander-tarasenko <tarasenkoA.dp@gmail.com>
2019-01-22 17:31:44 +0200

    fixed token interceptor for apollo-angular

--- client/src/app/graphql/graphql.module.ts
+++ client/src/app/graphql/graphql.module.ts
@@ -1,18 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { setContext } from 'apollo-link-context';
 import { environment } from './../../environments/environment';
 import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS, Apollo} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import { HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { ApolloLink } from 'apollo-link';
 
 const uri = `${environment.serverUrl}/graphql`; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
+  const http = httpLink.create({ uri });
+  
+  /* const auth = setContext(_ => {
+    return {headers: {Authorization: "1234"} }
+  }); */
+  const middleware = new ApolloLink((operation, forward) => {
+
+    // Check for token
+    const token = localStorage.getItem('token');
+    if (!token) return forward(operation);
+
+    operation.setContext({
+      headers: new HttpHeaders().set(
+        'Authorization',
+        token,
+      ),
+    });
+    return forward(operation);
+  });
+  const link = middleware.concat(http);
+  
   return {
-    link: httpLink.create({uri}),
+    link: link,
     cache: new InMemoryCache(),
   };
 }
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
   exports: [ApolloModule, HttpLinkModule],
   providers: [
     {
