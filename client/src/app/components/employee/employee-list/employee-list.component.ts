5460fa08bc2743d563b14feb7f718d872029314c
t-Zander <alex.sergeevich.tarasenko@gmail.com>
2019-01-23 22:19:02 +0200

    refactoring for signin and employees + graphQL

--- client/src/app/components/employee/employee-list/employee-list.component.ts
+++ client/src/app/components/employee/employee-list/employee-list.component.ts
@@ -1,41 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
 import { Employee } from 'src/app/shared/interfaces';
-import gql from 'graphql-tag';
 import { Router } from '@angular/router';
-
-export const GET_EMPLOYEES_LIST = gql`
-  query getEmployees{
-    employees{
-      _id
-      email
-      name
-      last_name
-      skills
-      company_id
-    }
-  }
-`;
+import { EmployeeGQLService } from '../services/employee-gql.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.scss']
 })
+
 export class EmployeeListComponent implements OnInit {
   employees: Employee[];
 
-  constructor(private apollo: Apollo, private router: Router) { }
+  constructor(private employeeGQLService: EmployeeGQLService, private router: Router) { }
 
   ngOnInit() {
-    this.apollo.watchQuery<{employees: Employee[]}>({
-      query: GET_EMPLOYEES_LIST
-    })
-    .valueChanges
-    .subscribe( ({data, loading}, ) => {
-      const {employees} = data;
-      this.employees = employees;
-    })
+    this.employeeGQLService
+      .getAllEmployees()
+      .subscribe( ({data, loading}) => {
+        const {employees} = data;
+        this.employees = employees;
+      });
   }
 
   onShowEmployeeInfo(employeeId: string) {
