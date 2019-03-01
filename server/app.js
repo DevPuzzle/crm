b43272019cf326664a7bbb6627f0148ae7f915ea
alex-fedurko <alex.fedurko.dp@gmail.com>
2019-03-01 12:12:38 +0200

    test pushing to master

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const cors = require('./middleware/cors');
 const path = require('path');
 
 const app = express();
-console.log('test')
+console.log('test') 
 app.use(bodyParser.json()); 
 app.use(cors);
 app.use(auth);
